e3074ff9269d32b7b7b609c1005efcecc7618dbd
sarath <tejamanchala200e@gmail.com>
2026-02-14 15:20:21 +0530

    Add SQLAlchemy engine options and upgrade database on app startup

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
 load_dotenv()
 from app import create_app
+from flask_migrate import upgrade
 app = create_app()
 
 if __name__ == '__main__':
+    with app.app_context():
+        upgrade()
     app.run(debug=True)
